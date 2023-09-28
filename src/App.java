4c2dc96fc2cca867be489b2bad174ac36e197232
Antonio Marcelo de Almeida <amalemida@gmail.com>
2023-09-27 22:47:09 -0300

    Merge Sort

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] array = {57,32,25,11,90,63};
+        int[] array = { 57, 32, 25, 11, 90, 63, 3, 8 };
 
+        
         Sort sort = new Sort();
+        sort.printArray(array);;
         sort.bubbleSort(array);
-        sort.printArray(array);    
+        System.out.println("Bubble Sort");
+        sort.printArray(array);
+        sort.selectionSort(array);
+        System.out.println("Selection Sort");
+        sort.printArray(array);
+        sort.mergeSort(array);
+        System.out.println( "Merge Sort");
+        sort.printArray(array);
     }
 
 }
--- src/Sort.java
+++ src/Sort.java
@@ -12,6 +12,48 @@ public class Sort {
         }
     }
 
+    public void selectionSort(int[] array) {
+        int i, j, min;
+
+        for (i = 0; i < array.length - 1; i++) {
+            min = i;
+            for (j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min])
+                    min = j;
+            }
+        }
+    }
+   
+    //criar mergeSort
+    public void mergeSort(int[] array) {
+        int[] aux = new int[array.length];
+        mergeSort(array, aux, 0, array.length - 1);
+    }
+    private void mergeSort(int[] array, int[] aux, int low, int high) {
+        if (high <= low)
+            return;
+        int mid = (low + high) / 2;
+        mergeSort(array, aux, low, mid);
+        mergeSort(array, aux, mid + 1, high);
+        merge(array, aux, low, mid, high);
+    }
+    private void merge(int[] array, int[] aux, int low, int mid, int high) {
+        for (int k = low; k <= high; k++)
+            aux[k] = array[k];
+        int i = low;
+        int j = mid + 1;
+        for (int k = low; k <= high; k++) {
+            if (i > mid)
+                array[k] = aux[j++];
+            else if (j > high)
+                array[k] = aux[i++];
+            else if (aux[j] < aux[i])
+                array[k] = aux[j++];
+            else
+                array[k] = aux[i++];
+        }
+    }
+
     public void printArray(int[] array) {
         System.out.print("[ ");
         for (int i = 0; i < array.length; i++) {
@@ -21,5 +63,4 @@ public class Sort {
         System.out.println("]");
         System.out.println();
     }
-
 }
